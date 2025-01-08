2658c21af4cded467d50be86b78a08a14afd571f
Sreekesh K Prabhu <145774649+sreekesh-k@users.noreply.github.com>
2025-01-08 14:35:46 +0530

    env setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
 import './App.css'
-
+import { APP_NAME } from './constants';
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <h1>Welcome to {APP_NAME}</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
