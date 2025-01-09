3b8f4b01080ce47b275422de243e4d9b4c626c7f
Sreekesh K Prabhu <145774649+sreekesh-k@users.noreply.github.com>
2025-01-09 13:05:59 +0530

    vendor app test

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import VendorApp from "./pages/VendorApp";
+import Vendor from "./pages/Vendor";
 
 function App() {
   return (
     <div>
       <VendorApp />
+      {/* <Vendor/> */}
     </div>
   );
 }
