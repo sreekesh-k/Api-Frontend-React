338281fceeb01bf7a9ffe41b53cfdfd4258d1e5d
Sreekesh K Prabhu <145774649+sreekesh-k@users.noreply.github.com>
2025-01-24 12:05:37 +0530

    styling

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "antd/dist/reset.css";
 import "./index.css";
-import "./react-layout.css";
-import './react-select.css';
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
