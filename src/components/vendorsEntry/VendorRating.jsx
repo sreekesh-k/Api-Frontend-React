c8056f3d41c80206d721c719cbb81d575d7da458
Sreekesh K Prabhu <145774649+sreekesh-k@users.noreply.github.com>
2025-01-24 10:18:42 +0530

    ratig fixes

--- src/components/vendorsEntry/VendorRating.jsx
+++ src/components/vendorsEntry/VendorRating.jsx
@@ -207,7 +207,7 @@ function VendorRating(props) {
     );
   };
   const saveRatingModel = (averageRatingodel) => {
-    console.log(averageRatingodel.ratingFile)
+    // console.log(averageRatingodel.ratingFile)
     dispatch(
       updateVendorRatingModel({
         Devaitions: averageRatingodel.devaitions,
@@ -805,7 +805,7 @@ function VendorRating(props) {
               {param.subParams !== null &&
                 param.subParams.map((sp, key) => {
                   return (
-                    <Row gutter={20}>
+                    <Row gutter={20} key={key}>
                       <Col
                         span="6"
                         style={{ marginTop: ".5rem", marginBottom: ".5rem" }}
@@ -1135,7 +1135,7 @@ function VendorRating(props) {
                     <path d="M16.5 6v11.5c0 2.21-1.79 4-4 4s-4-1.79-4-4V5a2.5 2.5 0 0 1 5 0v10.5c0 .55-.45 1-1 1s-1-.45-1-1V6H10v9.5a2.5 2.5 0 0 0 5 0V5c0-2.21-1.79-4-4-4S7 2.79 7 5v12.5c0 3.04 2.46 5.5 5.5 5.5s5.5-2.46 5.5-5.5V6h-1.5z"></path>
                   </svg>
                 </Button>
-              </Upload> 
+              </Upload>
             </Col>
           </Row>
         </Fragment>
